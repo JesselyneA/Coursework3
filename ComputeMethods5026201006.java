7651b0e934b1b68798e84835ea7e0d285eded840
JesselyneA <81258633+JesselyneA@users.noreply.github.com>
2021-04-01 00:32:49 +0700

    Add files via upload

---
+++ ComputeMethods5026201006.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+import java.lang.Math;
+public class ComputeMethods5026201006{
+    public double fToC(double degreesF){
+    return 5/9.0 * (degreesF - 32);
+    }
+    public double hypotenuse(double a, double b){
+    return Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
+    }
+    public int roll(){
+    Random dice = new Random ();
+    return dice.nextInt(7) + dice.nextInt(7);
+    }
+}
+    
+    
